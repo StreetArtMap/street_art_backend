a4ee5ea8086f71abbcbb9a535721cb1198c9c1b8
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-03 15:36:21 -0600

    Test street art model

---
+++ spec/models/street_art_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe StreetArt, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :latitude }
+    it { should validate_presence_of :longitude }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zipcode }
+    it { should validate_presence_of :image_urls }
+  end
+end
