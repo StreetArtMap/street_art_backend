49b9439870ccac1636015a0cc89803f30051c909
Michael Gallup <Gallup93@icloud.com>
2020-09-05 13:38:00 -0600

    Update 'null' attributes for GraphQL user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,8 +1,8 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :username, String, null: true
-    field :email, String, null: true
+    field :username, String, null: false
+    field :email, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
