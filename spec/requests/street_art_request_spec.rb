e35ae78dbb7257ad1751cfa0295fded2c2ef2ede
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-05 17:41:43 -0600

    Rename test

--- spec/requests/street_art_request_spec.rb
+++ spec/requests/street_art_request_spec.rb
@@ -9,11 +9,9 @@ RSpec.describe "art requests", type: :request do
     user = User.create(username: "Hank Hill", email: "ProPAIN@aol.com")
     user.street_arts.create(latitude: "39.742043", longitude: "-104.991531", address: "123 fakeplace", city: "Denver", state: "CO", zipcode: "80128", image_urls: ["https://images.freeimages.com/images/large-previews/5b5/bristles-1173194.jpg"], description: "neat-o!", artist_name: "Larry", favorite: true, visited: true)
 
-    post "/graphql", params: {"query" => "{streetArts {id, latitude, longitude}"}
-
-    # post "/graphql", params: { "query" => "{ streetArts {id}}"} 
+    post "/graphql", params: {query: "{streetArts {id, latitude, longitude}}", operationName: nil, context: ""}
+    binding.pry
 
     response = JSON.parse(response)
-    require "pry";binding.pry
   end
 end
