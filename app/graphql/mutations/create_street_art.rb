c4c1db220e30c33877e5ff295de1162a5b5f7cf4
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-06 15:28:05 -0600

    Set default values for create street art
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>
    Co-Authored-By: Stella  <stella@hey.com>

--- app/graphql/mutations/create_street_art.rb
+++ app/graphql/mutations/create_street_art.rb
@@ -5,19 +5,18 @@ class Mutations::CreateStreetArt < Mutations::BaseMutation
   argument :city, String, required: true
   argument :state, String, required: true
   argument :zipcode, String, required: true
-  # argument :image_urls, String, required: true
+  argument :image_urls, String, required: true
   argument :description, String, required: false
   argument :artist_name, String, required: false
   argument :art_name, String, required: false
   argument :instagram_handle, String, required: false
-  argument :favorite, Boolean, required: true
-  argument :visited, Boolean, required: true
+  argument :favorite, Boolean, required: false
+  argument :visited, Boolean, required: false
   argument :user_id, Integer, required: true
 
-  field :street_art, Types::StreetArtType, null:false
+  type Types::StreetArtType
 
-  #need to add image_urls back in
-  def resolve(latitude:, longitude:, address:, city:, state:, zipcode:, description:, artist_name:, art_name:, favorite:, visited:, user_id:)
-    x = StreetArt.new(latitude: latitude, longitude: longitude, address: address, city: city, state: state, zipcode: zipcode, description: description, artist_name: artist_name, art_name: art_name, favorite: favorite, visited: visited, user_id: user_id)
+  def resolve(latitude:, longitude:, address:, city:, state:, zipcode:, image_urls:, description: nil, artist_name: nil, art_name: nil, instagram_handle: nil, favorite: false, visited: false, user_id:)
+    User.find(user_id).street_arts.create!(latitude: latitude, longitude: longitude, address: address, city: city, state: state, zipcode: zipcode, image_urls: image_urls, description: description, artist_name: artist_name, art_name: art_name, instagram_handle: instagram_handle, favorite: favorite, visited: visited)
   end
 end
