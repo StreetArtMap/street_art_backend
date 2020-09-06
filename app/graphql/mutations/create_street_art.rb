6cf4ac5f9f36d83ed2ba55e748fdcd0fd0870903
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-06 15:46:00 -0600

    Update requirements and defaults for mutation
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>
    Co-Authored-By: Stella  <stella@hey.com>

--- app/graphql/mutations/create_street_art.rb
+++ app/graphql/mutations/create_street_art.rb
@@ -1,10 +1,10 @@
 class Mutations::CreateStreetArt < Mutations::BaseMutation
-  argument :latitude, String, required: true
-  argument :longitude, String, required: true
-  argument :address, String, required: true
-  argument :city, String, required: true
-  argument :state, String, required: true
-  argument :zipcode, String, required: true
+  argument :latitude, String, required: false
+  argument :longitude, String, required: false
+  argument :address, String, required: false
+  argument :city, String, required: false
+  argument :state, String, required: false
+  argument :zipcode, String, required: false
   argument :image_urls, String, required: true
   argument :description, String, required: false
   argument :artist_name, String, required: false
@@ -16,7 +16,7 @@ class Mutations::CreateStreetArt < Mutations::BaseMutation
 
   type Types::StreetArtType
 
-  def resolve(latitude:, longitude:, address:, city:, state:, zipcode:, image_urls:, description: nil, artist_name: nil, art_name: nil, instagram_handle: nil, favorite: false, visited: false, user_id:)
+  def resolve(latitude: nil, longitude: nil, address: nil, city: nil, state: nil, zipcode: nil, image_urls:, description: nil, artist_name: nil, art_name: nil, instagram_handle: nil, favorite: false, visited: false, user_id:)
     User.find(user_id).street_arts.create!(latitude: latitude, longitude: longitude, address: address, city: city, state: state, zipcode: zipcode, image_urls: image_urls, description: description, artist_name: artist_name, art_name: art_name, instagram_handle: instagram_handle, favorite: favorite, visited: visited)
   end
 end
