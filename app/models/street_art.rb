247941980da4f10f687bcd6a20f7731e951f8028
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-06 15:45:48 -0600

    Change validates presence of in model
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>
    Co-Authored-By: Stella  <stella@hey.com>

--- app/models/street_art.rb
+++ app/models/street_art.rb
@@ -2,12 +2,6 @@ class StreetArt < ApplicationRecord
   before_save :validate_location
   belongs_to :user
   # serialize :image_urls, Array
-  validates_presence_of :latitude
-  validates_presence_of :longitude
-  validates_presence_of :address
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zipcode
   validates_presence_of :image_urls
 
   def validate_location
