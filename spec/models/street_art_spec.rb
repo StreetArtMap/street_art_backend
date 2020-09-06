22f671a02899737739160ba7db557d00023c0b11
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-06 15:45:37 -0600

    Update validation check
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>
    Co-Authored-By: Stella  <stella@hey.com>

--- spec/models/street_art_spec.rb
+++ spec/models/street_art_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 describe StreetArt, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :latitude }
-    it { should validate_presence_of :longitude }
-    it { should validate_presence_of :address }
-    it { should validate_presence_of :city }
-    it { should validate_presence_of :state }
-    it { should validate_presence_of :zipcode }
-    # it { should validate_presence_of :image_urls }
+    it { should validate_presence_of :image_urls }
   end
 
   describe 'relationships' do
