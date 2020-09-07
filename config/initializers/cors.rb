5bdf06a8ef9394c7045ebd22c6bb94a7a09c2f96
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-07 17:32:10 -0600

    Add comment for using cors in production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # in production change origins to heroku site
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
