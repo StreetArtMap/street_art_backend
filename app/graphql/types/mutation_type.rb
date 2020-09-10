3db586dd95ab6245e2405d49de0ed11e8a66e359
Michael Gallup <Gallup93@icloud.com>
2020-09-09 20:08:50 -0600

    Add favorite_street_art to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
     field :create_street_art, mutation: Mutations::CreateStreetArt
+    field :favorite_street_art, mutation: Mutations::FavoriteStreetArt
   end
 end
