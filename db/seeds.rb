ccefe59dbdbc79c3547c91bf06a8e32d4ac50eec
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-10 21:04:17 -0600

    Add seeds and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,158 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 user = FactoryBot.create(:user)
-15.times do
-  FactoryBot.create(:street_art, user: user)
-end
-# factory :street_art do
-#   association :user
-#   latitude { Faker::Address.latitude }
-#   longitude { Faker::Address.longitude }
-#   address { Faker::Address.street_address }
-#   city { Faker::Movies::LordOfTheRings.location }
-#   state { Faker::Address.state_abbr }
-#   zipcode { Faker::Address.zip }
-#   image_urls {[ Faker::Fillmurray.image, Faker::Fillmurray.image, Faker::Fillmurray.image ]}
-#   description { Faker::Movies::StarWars.quote }
-#   artist_name { Faker::Artist.name }
-# end
-# user = User.create(username: "Hank Hill", email: "ProPAIN@aol.com")
-#
-# user.street_arts.create(latitude: "39.7666", longitude: "104.9837", address: "RiNo", city: "Denver", state: "CO", zipcode: "80216", image_urls: ["https://cdn.5280.com/2017/04/RiNo-neighborhood-street-art.jpg"], description: "neat-o!", artist_name: "Larry", art_name: "cool-art-1", favorite: true, visited: true)
-# user.street_arts.create(latitude: "39.7527", longitude: "105.0017", address: "LoDo", city: "Denver", state: "CO", zipcode: "80202", image_urls: ["https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTW8zd6DEZ5UHfY8r8NJB-jEcpGAlbHlwg6Cg&usqp=CAU"], description: "neat-o!", artist_name: "Larry",  art_name: "cool-art-2", instagram_handle: "@insta", favorite: false, visited: true)
-# user.street_arts.create(latitude: "39.7581", longitude: "104.9874", address: "Five Points", city: "Denver", state: "CO", zipcode: "80216", image_urls: ["https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRe-qGNr3_dvdgNqE6g81qa4OTlSvmfbiGCRw&usqp=CAU"], description: "woww!", artist_name: "Sammi", instagram_handle: "@insta", favorite: true, visited: true)
+
+user.street_art.create!({
+  latitude: '39.743083',
+  longitude: '-104.972802',
+  address: '1122 E 17th Ave',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80218',
+  image_urls: '/public/isabella_thallas.png',
+  description:'This mural is helping families through tough times as the community mourns Isabella Thallas, who was senselessly gunned down last week.',
+  artist_name: 'Thomas Evans',
+  instagram_handle: 'detour303'
+  })
+
+user.street_art.create!({
+  latitude: '39.772892',
+  longitude: '-104.968413',
+  address: '3990 Franklin Street',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80205',
+  image_urls: "['/public/john_lewis.png']",
+  description:'Mr. #GoodTrouble , John Lewis, will always be a part of our lives because we wouldn’t be here without him and many others that stood stood up during the civil rights movement. Many of you are making that Good Trouble as I paint this now. RIP John Lewis',
+  artist_name: 'Thomas Evans',
+  instagram_handle: 'detour303'
+  })
+
+user.street_art.create!({
+  latitude: '39.740240',
+  longitude: '-104.964179',
+  address: '1919 Colfax Ave',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80218',
+  image_urls: "['/public/george_floyd.png']",
+  description:'Two black muralist (Myself and @hieroveiga ) coming together in Denver to let their art speak. Hoping to usher in the next generation of artists, and especially black artists, that will use their art to make change in society.',
+  artist_name: 'Thomas Evans',
+  instagram_handle: 'detour303'
+  })
+
+user.street_art.create!({
+  latitude: '39.740254',
+  longitude: '-104.966621',
+  address: '1717 E Colfax Ave',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80218',
+  image_urls: "['/public/boxing_nurse.png']",
+  description:'Nurse wearing boxing gloves.',
+  artist_name: 'Austin Zucchini-Fowler',
+  instagram_handle: 'austinzart'
+  })
+
+user.street_art.create!({
+  latitude: '39.730335',
+  longitude: '-104.997068',
+  address: '724 W 9th',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80204',
+  image_urls: "['/public/veterinarian.png']",
+  description:'This one goes out to all the veterinarian’s! Thank you for always taking care of our animals and especially for staying open and working through the challenges of the pandemic.',
+  artist_name: 'Austin Zucchini-Fowler',
+  instagram_handle: 'austinzart'
+  })
+
+user.street_art.create!({
+  latitude: '39.754113',
+  longitude: '-104.987099',
+  address: '1030 Park Avenue West',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80205',
+  image_urls: "['/public/kissing.png','/public/kissing2.png']",
+  description:'Couple kissing.',
+  art_name: 'Kim & Jesse',
+  artist_name: 'Jason Thielke',
+  instagram_handle: 'jason_thielke'
+  })
+
+user.street_art.create!({
+  latitude: '39.779995',
+  longitude: '-105.043884',
+  address: '4582 Tennyson Street',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80212',
+  image_urls: "['/public/pachamama.png','/public/pachamama2.png']",
+  description:'La Pachamama is responding. Time to reflect on how we have been living as a society. How can we begin to bring back balance and live harmonious with nature?',
+  artist_name: 'Luis Valle',
+  instagram_handle: 'el_chan_guri'
+  })
+
+user.street_art.create!({
+  latitude: '39.773844',
+  longitude: '-105.043855',
+  address: '4170 Tennyson Street',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80212',
+  image_urls: "['/public/butterfly.png']",
+  description:'Butterfly wings made of flowers, with a bulldog.',
+  artist_name: 'Patrick Kane McGregor',
+  instagram_handle: 'patrickkanemcgregor'
+  })
+
+user.street_art.create!({
+  latitude: '39.742397',
+  longitude: '-105.031576',
+  address: '1585 Julian Street',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80204',
+  image_urls: "['/public/bulldog_portrait.png']",
+  description:'Portrait of a bulldog.',
+  artist_name: 'Patrick Kane McGregor',
+  instagram_handle: 'patrickkanemcgregor'
+  })
+
+user.street_art.create!({
+  latitude: '39.741809',
+  longitude: '-105.029868',
+  address: '3253 West Conejos Place',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80204',
+  image_urls: "['/public/sheep_fish.png']",
+  description:'sheep + fish',
+  instagram_handle: 'jerryrugg'
+  })
+
+user.street_art.create!({
+  latitude: '39.739773',
+  longitude: '-104.960385',
+  address: '2220 East Colfax Ave',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80206',
+  image_urls: "['/public/hands.png', '/public/hand.png']",
+  description:'Honoring missed connections',
+  artist_name: 'Lauren Napolitano',
+  instagram_handle: 'peptalks'
+  })
+
+user.street_art.create!({
+  latitude: '39.736899',
+  longitude: '-104.947954',
+  address: '1300 Adams Street',
+  city: 'Denver',
+  state: 'CO',
+  zipcode: '80206',
+  image_urls: "['/public/helen.png', '/public/helen2.png', '/public/helen3.png']",
+  description:'With this building I wanted to create a chronological Greek narrative. The piece starts with an egg hatching, hinting at the myth of Leda and the Swan. The mural ends with an image of Helen of Troy and her sister.',
+  art_name: 'The birth of Helen and Clytemnestra',
+  artist_name: 'Nick Napoletano',
+  instagram_handle: 'napoletanoart'
+  })
