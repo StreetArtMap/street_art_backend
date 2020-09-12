72c94308fe683311fe85c2857b0a71ac3bafef16
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-12 14:40:33 -0600

    Add algorithm for rino coordinates
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>

--- app/poro/rino_tour.rb
+++ app/poro/rino_tour.rb
@@ -2,14 +2,16 @@ class RinoTour < Tour
   attr_reader :id, :name, :link
 
   def initialize
-    id = 2
-    name = "RiNo Tour"
-    link = navigation_link(rino_coordinates)
+    @id = 2
+    @name = "RiNo Tour"
+    @link = navigation_link(rino_coordinates)
   end
 
   def rino_coordinates
-    latitudes = StreetArt.where('address ILIKE ?', '%Colfax%').pluck(:latitude)
-    longitudes = StreetArt.where('address ILIKE ?', '%Colfax%').pluck(:longitude)
+    search_params = StreetArt.where('latitude >= ? AND latitude <= ? AND longitude <= ? AND longitude >= ?', '39.758000', '39.765150', '-104.986480', '-104.978620' )
+    latitudes = search_params.pluck(:latitude)
+    longitudes = search_params.pluck(:longitude)
     coordinates = latitudes.zip(longitudes)
+    # coordinates.shuffle.first(23)
   end
 end
