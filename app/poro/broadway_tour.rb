22226e520a36b60d5b7c8f3768cfed266e76173a
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-12 14:57:36 -0600

    Add SQL to get broadway coordinates
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>

--- app/poro/broadway_tour.rb
+++ app/poro/broadway_tour.rb
@@ -2,14 +2,14 @@ class BroadwayTour < Tour
   attr_reader :id, :name, :link
 
   def initialize
-    id = 3
-    name = "Broadway Tour"
-    link = navigation_link(broadway_coordinates)
+    @id = 3
+    @name = "Broadway Tour"
+    @link = navigation_link(broadway_coordinates)
   end
 
   def broadway_coordinates
-    latitudes = StreetArt.where('address ILIKE ?', '%Colfax%').pluck(:latitude)
-    longitudes = StreetArt.where('address ILIKE ?', '%Colfax%').pluck(:longitude)
+    latitudes = StreetArt.where('address ILIKE ?', '%Broadway%').pluck(:latitude)
+    longitudes = StreetArt.where('address ILIKE ?', '%Broadway%').pluck(:longitude)
     coordinates = latitudes.zip(longitudes)
   end
 end
