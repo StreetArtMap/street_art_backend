772f37375f2e825dbd8ae82fa6938e6b65d75c3b
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-14 19:59:41 -0600

    Add method to tour parent class that arranges coordinates

--- app/poro/tour.rb
+++ app/poro/tour.rb
@@ -3,4 +3,10 @@ class Tour
   def navigation_link(coordinates)
     NavigationService.new.get_tour(coordinates)
   end
+
+  def arrange_coordinates(coordinates)
+    coordinates.sort do |lat, lon|
+      lat[1] <=>  lon[1]
+    end
+  end
 end
