0f5ac37ca7a8a80ec8a10ba6e25902f199452cf9
Michael Gallup <Gallup93@icloud.com>
2020-10-13 17:44:08 -0600

    Add passwordDigest to user_type.rb

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :username, String, null: false
     field :email, String, null: false
+    field :passwordDigest, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
